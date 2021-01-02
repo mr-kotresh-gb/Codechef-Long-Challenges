a954ebed593f3616304eccd55bf46486b12b1f6b
BetaCoder <60075769+badascoder@users.noreply.github.com>
2021-01-02 08:06:36 +0530

    Create DIVTHREE.py

---
+++ Codechef Jan 2021/DIVTHREE.py
@@ -0,0 +1,10 @@
+# Question link ->    https://www.codechef.com/DEC20B/problems/FAIRELCT
+# Code By       ->    Kotresh G B
+# Date          ->    01/01/2021
+
+t = int(input())
+for i in range(t):
+    n, k, d = map(int, input().split())
+    a = list(map(int, input().split()))
+    p = sum(a) // k
+    print(min(p, d))
